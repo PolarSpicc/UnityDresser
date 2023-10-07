7b877c3f7555080ec72fb4f02be6d44beef21e82
Polar <146941083+PolarSpicc@users.noreply.github.com>
2023-10-07 02:15:32 -0700

    Update 1.3.0

--- Polar/UnityDresser/Editor/UnityDresser.cs
+++ Polar/UnityDresser/Editor/UnityDresser.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Animations;
+using VRC.SDK3.Dynamics.PhysBone;
+using VRC.SDK3.Dynamics.PhysBone.Components;
 
 public class UnityDresser : EditorWindow
 {
-    private const string ver = "1.2.0";
+    private const string ver = "1.3.0";
     private static GameObject avatarRef;
     private static GameObject clothingRef;
     private static string prefix;
@@ -185,6 +188,23 @@ public class UnityDresser : EditorWindow
             }
         }
         AddLog("Finished parenting bones.");
+
+        Thread.Sleep(100);
+        foreach (var bone in avatarBones)
+        {
+            var physcomp = bone.Value.GetComponent<VRCPhysBone>();
+
+            if (physcomp == null)
+                continue;
+
+            Undo.RecordObject(physcomp, "clothing");
+            foreach (var child in bone.Value.GetComponentsInChildren<Transform>())
+            {
+                if (child.name.StartsWith($"({prefix}) "))
+                    physcomp.ignoreTransforms.Add(child);
+            }
+        }
+        AddLog("Finished setting ignored transforms in physbones.");
 
         if (makeAnims)
             AddLog("Created toggle animations.");
